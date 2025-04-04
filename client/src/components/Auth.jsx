033be93684d263581584a3da10e5c2959e19aa2b
Mouhamadou Bane <banebachir596@gmail.com>
2025-04-03 21:00:03 -0400

    my -auth

---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,87 @@
+import React, { useRef, useState } from "react";
+
+// The Auth component handles user signup and login
+const Auth = ({ updateToken }) => {
+
+  // The "signup" state determines whether the form is in signup (true) or login (false) mode
+  const [signup, setSignup] = useState(true);
+
+  // Create references for the form fields
+  const firstNameRef = useRef(); 
+  const lastNameRef = useRef(); 
+  const emailRef = useRef(); // 
+  const passwordRef = useRef(); 
+
+  // Function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevents the default form submission behavior
+
+    console.log("Form was submitted"); // Logs a message to the console
+
+
+    try {
+      // Send a POST request to register the user
+      const response = await fetch(`http:"localhost:8080/users${signup ? "/signup" : "/login"}`,
+                             
+        {
+          method: "POST", // HTTP method
+          headers: {
+            "Content-Type": "application/json", // Headers for the request to indicate the data is JSON
+          },
+          body: JSON.stringify({
+
+            // Body of the request with values from the form fields
+            firstName: firstNameRef.current.value,
+            lastName: lastNameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+          }),
+        }
+      );
+
+      // Get the response as JSON
+      const data = await response.json();
+
+      console.log(data); // Log the response from the API
+
+      // Update the token with the value received in the response
+      updateToken(data.Token);
+    } catch (err) {
+      console.log(err); // Log any errors
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {" "}
+                          {/* When the form is submitted, call handleSubmit */}
+      <h2>{signup ? "Signup" : "Login"}</h2>{" "}
+                           {/* Display "Signup" or "Login" based on the signup state */}
+      {signup && (
+        <>
+                       {/* First Name and input LiastName*/}
+          <input ref={firstNameRef} placeholder="First Name" required />{" "}
+          
+          <input ref={lastNameRef} placeholder="Last Name" required />{" "}
+          
+        </>
+      )}
+                         {/* input email / input password / submit button  */}
+      <input ref={emailRef} placeholder="Email" required /> 
+      <input ref={passwordRef} placeholder="Password" required />{" "}
+      <button>Submit</button> {/* Submit button */}
+      <button
+        type="button"
+        onClick={() => {
+          // When clicked, toggle the "signup" state to switch between signup and login
+          setSignup(!signup);
+        }}
+      >
+        {/* Button text changes based on the "signup" state */}
+        {signup ? "Need to login?" : "Need to signup?"}
+      </button>
+    </form>
+  );
+};
+
+export default Auth; // Export the component to be used in other files
